58762e2cf8dcd675cc3db0bdc7124380190f8d3e
Stefano Locati <stivlo@gmail.com>
2019-05-14 14:11:32 +0100

    95. Adding Signup to the web app

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -7,12 +7,23 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import { User } from './user.model';
 
+import { CognitoUserPool, CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js';
+
+const POOL_DATA = {
+  UserPoolId: 'us-east-1_ur59q5oW0',     // Cognito | User Pools | compare-yourself | Pool Id
+  ClientId: '4krk787sjaq77gq2pf8hfi47jl' // Cognito | User Pools | compare-yourself | App Clients | App client id
+};
+const userPool = new CognitoUserPool(POOL_DATA);
+
 @Injectable()
 export class AuthService {
   authIsLoading = new BehaviorSubject<boolean>(false);
   authDidFail = new BehaviorSubject<boolean>(false);
   authStatusChanged = new Subject<boolean>();
+  registeredUser: CognitoUser; 
+
   constructor(private router: Router) {}
+
   signUp(username: string, email: string, password: string): void {
     this.authIsLoading.next(true);
     const user: User = {
@@ -24,8 +35,20 @@ export class AuthService {
       Name: 'email',
       Value: user.email
     };
+    const attrList: CognitoUserAttribute[] = [];
+    attrList.push(new CognitoUserAttribute(emailAttribute));
+    userPool.signUp(user.username, user.password, attrList, null, (err, result) => {
+      this.authIsLoading.next(false);
+      if (err) {
+        this.authDidFail.next(true);
+        return;
+      }
+      this.authDidFail.next(false);
+      this.registeredUser = result.user;
+    });
     return;
   }
+
   confirmUser(username: string, code: string) {
     this.authIsLoading.next(true);
     const userData = {
